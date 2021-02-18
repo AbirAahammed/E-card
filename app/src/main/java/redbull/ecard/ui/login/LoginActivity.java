4a566c16fcf8caf189bb84d0337dd96a19b7818e
abirAsakib <ahammed.abir47@gmail.com>
2021-02-18 16:56:03 -0600

    Add accrount creation option using Firebase auth: Issue: #35 Task completed : - User can sign up for new account - Upon using same email user propmted to login

--- app/src/main/java/redbull/ecard/ui/login/LoginActivity.java
+++ app/src/main/java/redbull/ecard/ui/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -23,25 +24,46 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+import redbull.ecard.MainActivity;
 import redbull.ecard.R;
 import redbull.ecard.ui.login.LoginViewModel;
 import redbull.ecard.ui.login.LoginViewModelFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final  String TAG = "LoginActivity";
+    private FirebaseAuth firebaseAuth;
     private LoginViewModel loginViewModel;
+
+    int AUTHUI_REQUEST_CODE = 10001;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         setTitle("Sign IN");
         super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
+
+        // check if there is any logged in user, if so then go to main landing page
+        if (firebaseAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            this.finish();
+        }
+
         loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
+        final Button loginButton = findViewById(R.id.button_login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
@@ -122,6 +144,51 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Handle both login and account creation using FirebaseAuthUI
+     * @param the view from which the method is called
+     * @return void type*/
+    public void handleLoginRegister(View view) {
+        List<AuthUI.IdpConfig> provider = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+        Intent intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(provider)
+                .setTosAndPrivacyPolicyUrls("https://example.com", "https://example.com")
+                .setLogo(R.drawable.common_google_signin_btn_icon_dark)
+                .build();
+        startActivityForResult(intent, AUTHUI_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == AUTHUI_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // we have signed in the iser or we have a new user
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d(TAG, "onActivityResult: "+ user.getEmail());
+                if (user.getMetadata().getCreationTimestamp() == user.getMetadata().getLastSignInTimestamp()) {
+                    Toast.makeText(this, "Welcome new User", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Welcome back again", Toast.LENGTH_LONG).show();
+                }
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                this.finish();
+            } else {
+                // Sign in failed
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+                if (response == null) {
+                    Log.d(TAG, "onActivityResult: the user has canceled the sign in request");
+                } else {
+                    Log.d(TAG, "onActivityResult: ", response.getError());
+                }
+            }
+
+        }
+    }
     private void updateUiWithUser(LoggedInUserView model) {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
