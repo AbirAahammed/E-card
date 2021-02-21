836a4ca99dbfca1fc19f7141925e529e4e9bdce1
abirAsakib <ahammed.abir47@gmail.com>
2021-02-20 18:42:06 -0600

    File structure refactoring for LoginActivityTest.java Issue: #38, #26 Task Completed : - A sample test was created

--- app/src/androidTest/java/redbull/ecard/UILayerTest/login/LoginActivityTest.java
+++ app/src/androidTest/java/redbull/ecard/UILayerTest/login/LoginActivityTest.java
@@ -1,7 +1,10 @@
 package redbull.ecard.UILayerTest.login;
 
+import android.util.Log;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -11,13 +14,13 @@ import org.junit.runner.RunWith;
 import redbull.ecard.UILayer.login.LoginActivity;
 
 @LargeTest
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class LoginActivityTest {
     @Rule
     public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
 
     @Test
     public void loginActivityTest() {
-
+        // Write your UI test here
     }
 }
