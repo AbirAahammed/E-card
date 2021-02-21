0962acbad40b145ed639719462503e42aa935f9b
abirAsakib <ahammed.abir47@gmail.com>
2021-02-20 18:18:47 -0600

    File structure refactoring for LoginActivityTest.java Issue: #38 Task Completed : - Compile error fixed - All UI test are now under android test package.

--- app/src/androidTest/java/redbull/ecard/UILayerTest/login/LoginActivityTest.java
+++ app/src/androidTest/java/redbull/ecard/UILayerTest/login/LoginActivityTest.java
@@ -1,22 +1,23 @@
-package redbull.ecard.UILayer.login;
+package redbull.ecard.UILayerTest.login;
 
-
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import redbull.ecard.UILayer.login.LoginActivity;
+
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
-
     @Rule
     public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
 
     @Test
     public void loginActivityTest() {
+
     }
 }
