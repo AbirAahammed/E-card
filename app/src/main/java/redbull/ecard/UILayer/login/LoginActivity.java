4c39204a7d78963f02d91d66d8aaf10c80e1631a
abirAsakib <ahammed.abir47@gmail.com>
2021-02-21 22:10:10 -0600

    Account login Issue: #12, #27 Task Completed : - Regular login using email and password

--- app/src/main/java/redbull/ecard/UILayer/login/LoginActivity.java
+++ app/src/main/java/redbull/ecard/UILayer/login/LoginActivity.java
@@ -1,17 +1,14 @@
 package redbull.ecard.UILayer.login;
 
 import android.app.Activity;
-
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -23,17 +20,18 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 import java.util.Arrays;
 import java.util.List;
-
 import redbull.ecard.MainActivity;
 import redbull.ecard.R;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -134,17 +132,46 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final EditText usernameEditText = findViewById(R.id.username);
+                final EditText passwordEditText = findViewById(R.id.password);
+                Log.i(TAG, "Username : "+usernameEditText.getText().toString());
+                Log.i(TAG, "Password : "+passwordEditText.getText().toString());
                 Log.i("ABIR_DEV_TAG","SignIN Clicked");
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
+
+                // Firebase authentication using Email and password initiating
+                firebaseAuth.signInWithEmailAndPassword(usernameEditText.getText().toString(),
+                        passwordEditText.getText().toString()).addOnCompleteListener(
+                                LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Log.d(TAG, "onActivityResult: "+ user.getEmail());
+                            Toast.makeText(LoginActivity.this, "Welcome new User",
+                                    Toast.LENGTH_LONG).show();
+
+                            Intent intent = new Intent(LoginActivity.this,
+                                    MainActivity.class);
+                            startActivity(intent);
+                            LoginActivity.this.finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            // ...
+                        }
+                    }
+                });
             }
         });
     }
 
     /**
      * Handle both login and account creation using FirebaseAuthUI
-     * @param  the view from which the method is called
+     * @param  /the view from which the method is called
      * @return void type
      * */
     public void handleLoginRegister(View view) {
@@ -154,12 +181,14 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = AuthUI.getInstance()
                 .createSignInIntentBuilder()
                 .setAvailableProviders(provider)
-                .setTosAndPrivacyPolicyUrls("https://example.com", "https://example.com")
+                .setTosAndPrivacyPolicyUrls("https://example.com",
+                        "https://example.com")
                 .setLogo(R.drawable.common_google_signin_btn_icon_dark)
                 .build();
         startActivityForResult(intent, AUTHUI_REQUEST_CODE);
 
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -169,7 +198,8 @@ public class LoginActivity extends AppCompatActivity {
                 // we have signed in the iser or we have a new user
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 Log.d(TAG, "onActivityResult: "+ user.getEmail());
-                if (user.getMetadata().getCreationTimestamp() == user.getMetadata().getLastSignInTimestamp()) {
+                if (user.getMetadata().getCreationTimestamp() == user.getMetadata()
+                        .getLastSignInTimestamp()) {
                     Toast.makeText(this, "Welcome new User", Toast.LENGTH_LONG).show();
                 } else {
                     Toast.makeText(this, "Welcome back again", Toast.LENGTH_LONG).show();
@@ -189,6 +219,8 @@ public class LoginActivity extends AppCompatActivity {
 
         }
     }
+
+
     private void updateUiWithUser(LoggedInUserView model) {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
