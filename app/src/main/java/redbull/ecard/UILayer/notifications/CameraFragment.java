34787c5cd6dd83ee1041a24da30912f8fc0917d6
h-connor <62314329+h-connor@users.noreply.github.com>
2021-02-23 18:55:50 -0600

    Updating Branch
    Issue #46
    
    - Does not resolve the issue
    Updating the branch for exchange with another developer. This is so the branch contains the information in Develop, as well as some recent prototype changes.

---
+++ app/src/main/java/redbull/ecard/UILayer/notifications/CameraFragment.java
@@ -0,0 +1,35 @@
+package redbull.ecard.UILayer.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import redbull.ecard.R;
+
+public class CameraFragment extends Fragment {
+
+    private NotificationsViewModel notificationsViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_camera, container, false);
+        final TextView textView = root.findViewById(R.id.camera_view);
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
