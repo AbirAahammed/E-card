f453c2251c282f2ec65ed0babde7bd36505aef1b
ConnorGehman <73304914+ConnorGehman@users.noreply.github.com>
2021-02-25 00:19:39 -0600

    Deleted dummy class (to commit directory) as it was no longer needed
    Issue #50

--- app/src/main/java/redbull/ecard/PersistenceLayer/PersistenceDummyClass.java
+++
@@ -1,7 +0,0 @@
-package redbull.ecard.PersistenceLayer;
-
-public class PersistenceDummyClass {
-    /*
-     * This file is temporary. The persistence layer would not be commited without something in it.
-     */
-}
