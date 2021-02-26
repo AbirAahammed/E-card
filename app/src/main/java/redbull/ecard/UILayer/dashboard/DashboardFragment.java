bf1fe01766b918238b3a1cb53215446a36c09691
Chenbo Ma <chenboma@hotmail.com>
2021-02-26 00:06:42 -0600

    Profile page(working on it)
    profile page

--- app/src/main/java/redbull/ecard/UILayer/dashboard/DashboardFragment.java
+++ app/src/main/java/redbull/ecard/UILayer/dashboard/DashboardFragment.java
@@ -1,29 +1,62 @@
 package redbull.ecard.UILayer.dashboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import redbull.ecard.R;
+import android.app.Activity;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import java.util.Arrays;
+import java.util.List;
+import redbull.ecard.MainActivity;
+import redbull.ecard.R;
 
-public class DashboardFragment extends Fragment {
+import android.widget.Button;
+import android.widget.ImageView;
 
+public class DashboardFragment extends Fragment{
     private DashboardViewModel dashboardViewModel;
-
+    public ImageView hImageViewSemafor;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        Log.d("CREATFRAG","SD");
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-
+        View root = inflater.inflate(R.layout.activity_dashboard, container, false);
         return root;
     }
 }
+
