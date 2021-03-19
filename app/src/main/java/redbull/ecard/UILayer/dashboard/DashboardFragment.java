fa81a7e233fe0b1f29fd958bccec8fab7376a5b1
Chenbo Ma <chenboma@hotmail.com>
2021-03-19 15:29:37 -0500

    QR image
    Now we could generate QR!

--- app/src/main/java/redbull/ecard/UILayer/dashboard/DashboardFragment.java
+++ app/src/main/java/redbull/ecard/UILayer/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package redbull.ecard.UILayer.dashboard;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,14 +13,38 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 import redbull.ecard.R;
 
-public class DashboardFragment extends Fragment {
 
+import redbull.ecard.R;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+public class DashboardFragment extends Fragment {
+    private ImageView qrCodeIV;
+    private EditText dataEdt;
+    Bitmap bitmap;
+    QRGEncoder qrgEncoder;
     private DashboardViewModel dashboardViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        qrCodeIV.findViewById(R.id.imageView2);
         dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
