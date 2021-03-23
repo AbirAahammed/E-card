bd3044c2ccf9ae4cfd7bf6c26df3edd514b28648
Tianlei Wang <leilei980414@gmail.com>
2021-03-22 22:40:35 -0500

    update the MainActivityTest and added mockito

--- app/src/androidTest/java/redbull/ecard/TravisCiTests/MainActivityTest.java
+++ app/src/androidTest/java/redbull/ecard/TravisCiTests/MainActivityTest.java
@@ -1,28 +1,63 @@
 package redbull.ecard.TravisCiTests;
 
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
-import androidx.test.filters.SmallTest;
 
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import redbull.ecard.DataLayer.testData.testID;
 import redbull.ecard.MainActivity;
 import redbull.ecard.R;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @LargeTest
 public class MainActivityTest {
     @Rule
-    public ActivityScenarioRule<MainActivity> intentsTestRule =
+    public ActivityScenarioRule<MainActivity> rule =
             new ActivityScenarioRule<MainActivity>(MainActivity.class);
-
+    @Before
+    public void setup(){
+        FirebaseAuth auth= FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(testID.email, testID.password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = auth.getCurrentUser();
+                    assertNotNull("failed with login",user);
+                }
+                else {
+                    System.out.println("Test failed because failed with login");
+                    System.out.flush();
+                    fail();
+                }
+            }}
+        );
+    }
     @Test
     public void fragmentTest(){
-        onView(withId(R.id.navigation_home)).perform(click());
+
+       onView(withId(R.id.navigation_home)).perform(click());
         onView(withId(R.id.navigation_notifications)).perform(click());
         onView(withId(R.id.navigation_dashboard)).perform(click());
     }
