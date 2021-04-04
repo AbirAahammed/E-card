4f02869d97d86c4ed113bf7c7d885075bc40d000
abirAsakib <ahammed.abir47@gmail.com>
2021-04-03 19:24:01 -0500

    Code refactoring comleted on 2 files Issue: N/A Task Completed: - Methods are well documented - unnecessary imports are removed

--- app/src/main/java/redbull/ecard/PersistenceLayer/PersistenceInterface.java
+++ app/src/main/java/redbull/ecard/PersistenceLayer/PersistenceInterface.java
@@ -2,12 +2,39 @@ package redbull.ecard.PersistenceLayer;
 
 import redbull.ecard.DataLayer.Model;
 
+/**
+ * PersistenceInterface.java
+ * An interface for all persistence object in ECard Project
+ * @author abir ahammed
+ * @since 03-15-2021
+ */
 interface PersistenceInterface {
 
+    /**
+     * Creates an entry given the required object
+     * @param model
+     */
     public void create(Model model);
+
+    /**
+     * Reads an entry in firebase realtime db given the uid as a string.
+     * @param uid
+     * @return An instance of itself to attach the listener in a single statement
+     */
     public PersistenceInterface read(String uid);
+
+    /**
+     * Updates an entry with the updated value of model
+     * @param model
+     */
     public void update(Model model);
+
+    /**
+     * Deletes an entry with the uid as a string
+     * @param id
+     */
     public void delete(Long id);
+
 }
 
 
--- app/src/main/java/redbull/ecard/PersistenceLayer/ProfilePersistence.java
+++ app/src/main/java/redbull/ecard/PersistenceLayer/ProfilePersistence.java
@@ -8,7 +8,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import redbull.ecard.DataLayer.Address;
@@ -16,10 +15,14 @@ import redbull.ecard.DataLayer.Contact;
 import redbull.ecard.DataLayer.Model;
 import redbull.ecard.DataLayer.Name;
 import redbull.ecard.DataLayer.Profile;
-import redbull.ecard.DataLayer.Services;
 import redbull.ecard.PersistenceLayer.Listeners.OnReadCompleteListener;
 
-
+/**
+ * The entry point for creating, accessing or updating a Profile. You can get an instance by calling {@link
+ * ProfilePersistence#getInstance()}. 
+ * To create a profile {@link ProfilePersistence#create(Model)}
+ * To read a profile {@link ProfilePersistence#read(String)}
+ */
 public class ProfilePersistence implements PersistenceInterface  {
     private static final String TABLENAME  = "profiles";
     private OnReadCompleteListener readListener;
@@ -30,26 +33,51 @@ public class ProfilePersistence implements PersistenceInterface  {
         this.dbTableRef = firebaseDatabaseInstance.getReference(TABLENAME);
     }
 
+
+    /**
+     * A default instance of ProfilePersistence
+     *
+     * @return An instance of ProfilePersistenct
+     */
     public static  ProfilePersistence getInstance() {
         return new ProfilePersistence(FirebaseDatabase.getInstance());
     }
+
+    /**
+     * Sets Local Persistence value to input value, if set to TRUE the firebase data is stored in
+     * device persistence.
+     *
+     * @param bool Value to set for local persistence
+     */
     public void setLocalPersistenceEnabled(boolean bool){
         firebaseDatabaseInstance.setPersistenceEnabled(bool);
     }
 
+
+    /**
+     * turn on or off Firebase DB sync with local storage for auto syncing.
+     * @param bool Value to set for local data sync with Firebase
+     */
     public void setPersistenceSync(boolean bool) {
         this.dbTableRef.keepSynced(bool);
     }
+
+
+    /**
+     * Creates a Profile  given the required profile object
+     *
+     * Sets the profile name using {@link #createUserName(DatabaseReference, Name)}
+     * Sets the profile address using {@link #createUserAddress(DatabaseReference, Address)}
+     * @param model A profile object
+     */
     @Override
     public void create(Model model) {
-        // do a check before casting
         if (model instanceof Profile){
             Profile profile = (Profile) model;
             createUserName(dbTableRef.child(profile.getUID()).child("name"), profile.getName());
             createUserAddress(dbTableRef.child(profile.getUID()).child("address"), profile.getAddress());
             createUserContact(dbTableRef.child(profile.getUID()).child("contact"), profile.getContact());
             createUserDescription(dbTableRef.child(profile.getUID()).child("description"), profile.getDescription());
-//            createUserServices(dbTableRef.child(profile.getUID()).child("serviceIndexes"), profile.getServices());
             dbTableRef.child(profile.getUID()).child("service").setValue(profile.getService());
         }
         else {
@@ -58,11 +86,24 @@ public class ProfilePersistence implements PersistenceInterface  {
         }
     }
 
+    /**
+     * Sets the name of the profile given the name object
+     *
+     * @param dbUserNameRef firebase db refrence for the name instnace url
+     * @param name name object to set
+     */
     private void createUserName(DatabaseReference dbUserNameRef, Name name){
         dbUserNameRef.child("firstName").setValue(name.getFirstName());
         dbUserNameRef.child("lastName").setValue(name.getLastName());
         dbUserNameRef.child("middleName").setValue(name.getMiddleName());
     }
+
+    /**
+     * Sets the Users address under the profile designated by dbUserAddressRef
+     *
+     * @param dbUserAddressRef firebase table ref
+     * @param address the address object
+     */
     private void createUserAddress(DatabaseReference dbUserAddressRef, Address address){
         dbUserAddressRef.child("roadNumber").setValue(address.getRoadNumber());
         dbUserAddressRef.child("houseNumber").setValue(address.getHouseNumber());
@@ -71,22 +112,37 @@ public class ProfilePersistence implements PersistenceInterface  {
         dbUserAddressRef.child("province").setValue(address.getProvince());
         dbUserAddressRef.child("country").setValue(address.getCountry());
     }
+
+    /**
+     * Sets the Users contact under the profile designated by dbUserContactRef
+
+     * @param dbUserContactRef firebase db reference for contact
+     * @param contact
+     */
     private void createUserContact(DatabaseReference dbUserContactRef, Contact contact){
         dbUserContactRef.child("cellPhone").setValue(contact.getCellPhone());
         dbUserContactRef.child("homePhone").setValue(contact.getHomePhone());
         dbUserContactRef.child("emailAddress").setValue(contact.getEmailAddress());
     }
+
+    /**
+     * Sets the Users description under the profile designated by dbUserDescriptionRef
+
+     * @param dbUserDescriptionRef firebase db reference for description
+     * @param description the description as a string
+     */
     private void createUserDescription(DatabaseReference dbUserDescriptionRef, String description) {
         dbUserDescriptionRef.child("description").setValue(description);
     }
 
-//    TODO NO required in the way currently service implemented as we only have one service for one person
-//    private void createUserServices(DatabaseReference dbUserServiceRef, Services services) {
-//        // Creates a string of comma-separated indexes that match the indexes of the ServicesTypes enum.
-//        String serviceIndexesDBFormat = services.getIndexesInDBFormat();
-//        dbUserServiceRef.child("serviceIndexes").setValue(serviceIndexesDBFormat);
-//    }
 
+    /**
+     * Reads the profile of the UID string given. It returns an instance of itself and then caller
+     * have to latch a listener to the returned object
+     *
+     * @param uid the uid of the profile
+     * @return itself
+     */
     @Override
     public PersistenceInterface read(String uid) {
         final Profile profile = new Profile();
@@ -105,14 +161,28 @@ public class ProfilePersistence implements PersistenceInterface  {
         return this;
     }
 
+    /**
+     * Adds the listener to this instance
+     *
+     * @param onReadCompleteListener an instance of {@link OnReadCompleteListener}
+     */
     public void addOnProfileReadCompleteListener(OnReadCompleteListener onReadCompleteListener){
         this.readListener = onReadCompleteListener;
     }
+
+    /**
+     * Not implemented
+     * @param model
+     */
     @Override
     public void update(Model model) {
 
     }
 
+    /**
+     * Not implemented
+     * @param id
+     */
     @Override
     public void delete(Long id) {
 
