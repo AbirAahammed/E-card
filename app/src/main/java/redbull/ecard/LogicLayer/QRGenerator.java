4e8516175c8dfc4bff505e618d614a7d880c5880
Chenbo Ma <chenboma@hotmail.com>
2021-04-08 15:39:43 -0500

    Modify QRgenerator
    Modify a little QRgenerator

--- app/src/main/java/redbull/ecard/LogicLayer/QRGenerator.java
+++ app/src/main/java/redbull/ecard/LogicLayer/QRGenerator.java
@@ -1,5 +1,21 @@
 package redbull.ecard.LogicLayer;
 
+import android.app.Activity;
+import android.widget.ImageView;
+
+import com.google.firebase.firestore.core.View;
+
+import redbull.ecard.R;
+
 public class QRGenerator {
+    private static android.view.View rootView;
+    public QRGenerator(android.view.View rootView){
+        this.rootView = rootView;
+    }
+
+    public ImageView getPos(View a){
+        return (ImageView) rootView.findViewById(R.id.imageView2);
+    }
+
 
 }
